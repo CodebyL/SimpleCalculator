257bf23eccf2373bb004c22573b60ff7ed7a8c71
lisettecolon <lisettecolon@localhost>
2023-11-20 10:07:55 -0500

    first commit!

---
+++ src/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simplecalculator;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.control.Button;
+import javafx.control.Label;
+import javafx.layout, GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author lisettecolon
+ */
+public class SimpleCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
